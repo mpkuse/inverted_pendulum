a006c4ec0d4ec9dce7201a46a4fe0caa00dcd0a6
mpkuse <mpkuse@connect.ust.hk>
2019-02-10 17:40:26 +0800

    fixed the linearization

--- analysis_of_linearization.py
+++ analysis_of_linearization.py
@@ -8,14 +8,17 @@ M = 5.0
 d1 = 1.0
 d2 = 0.5
 
-# Pendulum up
+# Pendulum up (linearized eq)
 # Eigen val of A : array([[ 1.        , -0.70710678, -0.07641631,  0.09212131] )
+_q = (m+M) * g / (M*L)
 A = np.array([\
             [0,1,0,0], \
             [0,-d1, -g*m/M,0],\
-            [0,0,0,1],\
-            [0,0,(M+m)*g/(M*L),-d2] ] )
-B = np.array( [ [0, 0 ],[1./M, g*m*np.pi/(2*M) ], [0,0], [-1./(M*L), -g*(m+M)*np.pi / (2*M*L) ]  ])
+            [0,0,0,1.],\
+            [0,d1/L,_q,-d2] ] )
+
+B = np.expand_dims( np.array( [0, 1.0/M, 0., -1/(M*L)] ), 1 )
+
 
 
 # Pendulum Down - Verified correct.
@@ -26,5 +29,21 @@ B = np.array( [ [0, 0 ],[1./M, g*m*np.pi/(2*M) ], [0,0], [-1./(M*L), -g*(m+M)*np
 #             [0,0,0,1],\
 #             [0,0,-(M+m)*g/(M*L),-d2] ] )
 
-print np.linalg.matrix_rank( control.ctrb( A, B ) )
-print np.linalg.eig( A )
+#B = np.array( [] )
+
+print 'A\n', A
+print 'B\n', B
+
+# Controllability
+print '---Controllability'
+print 'rank of ctrb(A,b)' , np.linalg.matrix_rank( control.ctrb( A, B ) )
+print 'Eigenvalues of A ', np.linalg.eig( A )
+
+
+# Pole Placement
+K = control.place( A, B, [-1, -2, -4, -5] )
+print '---Pole Placement\nK=\n', K
+
+# Verification of Eigen values of A-BK
+print '---Verification of Eigen values of A-BK'
+print 'Eigenvalues of A-BK', np.linalg.eig( A-np.matmul(B,K) )
--- controlled_cart_and_pendulum.py
+++ controlled_cart_and_pendulum.py
@@ -4,123 +4,100 @@ import cv2
 from InvertedPendulum import InvertedPendulum
 
 from scipy.integrate import solve_ivp
-import scipy.linalg
+import control
 import code
 
-def lqr(A,B,Q,R):
-    """Solve the continuous time lqr controller.
 
-    dx/dt = A x + B u
+class MyLinearizedSystem:
+    def __init__(self):
+        g = 9.8
+        L = 1.5
+        m = 1.0
+        M = 5.0
+        d1 = 1.0
+        d2 = 0.5
 
-    cost = integral x.T*Q*x + u.T*R*u
-    Implementation borrowed from : http://www.kostasalexis.com/lqr-control.html
-    """
-    #ref Bertsekas, p.151
+        # Pendulum up (linearized eq)
+        # Eigen val of A : array([[ 1.        , -0.70710678, -0.07641631,  0.09212131] )
+        _q = (m+M) * g / (M*L)
+        self.A = np.array([\
+                    [0,1,0,0], \
+                    [0,-d1, -g*m/M,0],\
+                    [0,0,0,1.],\
+                    [0,d1/L,_q,-d2] ] )
 
-    #first, try to solve the ricatti equation
-    X = np.matrix(scipy.linalg.solve_continuous_are(A, B, Q, R))
+        self.B = np.expand_dims( np.array( [0, 1.0/M, 0., -1/(M*L)] ) , 1 ) # 4x1
 
-    #compute the LQR gain
-    K = np.matrix(np.linalg.inv(R)*(B.T*X))
-    # K = np.matrix( B.T*X )
+    def compute_K(self, desired_eigs = [-0.1, -0.2, -0.3, -0.4] ):
+        self.K = control.place( self.A, self.B,  desired_eigs )
 
-    eigVals, eigVecs = scipy.linalg.eig(A-B*K)
-
-    return K, X, eigVals
-
-def dlqr(A,B,Q,R):
-    """Solve the discrete time lqr controller.
+    def get_K(self):
+        return self.K
 
 
-    x[k+1] = A x[k] + B u[k]
-
-    cost = sum x[k].T*Q*x[k] + u[k].T*R*u[k]
-    """
-    #ref Bertsekas, p.151
-
-    #first, try to solve the ricatti equation
-    X = np.matrix(scipy.linalg.solve_discrete_are(A, B, Q, R))
-
-    #compute the LQR gain
-    K = np.matrix(scipy.linalg.inv(B.T*X*B+R)*(B.T*X*A))
-
-    eigVals, eigVecs = scipy.linalg.eig(A-B*K)
-
-    return K, X, eigVals
-
-# if __name__=="__1main__":
-#     print "Test lqr"
-#     g = 9.8
-#     L = 1.5
-#     m = 1.0
-#     M = 5.0
-#     A = np.array([\
-#                 [0,1,0,0], \
-#                 [0,-1./M,-g*m/M,0],\
-#                 [0,0,0,1],\
-#                 [0,-1./(M*L),(M+m)*g/(M*L),0] ] )
-#     B = np.array( [ [0],[1./M],[0],[1./(M*L)]  ])
-#
-#     _a, _b, _c = lqr( A, B, np.eye(4), 1. )
-#     code.interact( local=locals() )
+# Global Variables
+ss = MyLinearizedSystem()
+ss.compute_K(desired_eigs = np.array([-10.1, -10.2, -10.3, -10.4])*0.000001 )
 
 
 
-# This will be our LQR Controller. Note that this is not a PID Controller.
+
+# This will be our LQR Controller.
 # LQRs are more theoritically grounded, they are a class of optimal control algorithms.
 # The control law is u = KY. K is the unknown which is computed as a solution to minimization problem.
 def u( t , y ):
-    g = 9.8
-    L = 1.5
-    m = 1.0
-    M = 5.0
-    A = np.array([\
-                [0, 1, 0                      ,0], \
-                [0, -1      , -g*m/M       ,0],\
-                [0, 0          , 0            ,1],\
-                [0, 0, (M+m)*g/(M*L)   ,-0.5] ] )
+    u_ = -np.matmul( ss.K , y )
+    print 'u()', t, u_
+    # code.interact(local=dict(globals(), **locals()))
+    return 0.1
+    # return u_[0]
 
-    B = np.array( [ [0,0],[1./M,g*m*np.pi/(2.*M)],[0,0],[-1./(M*L), -g*(m+M)*np.pi/(2*M*L)]  ])
-
-    K, _b, _c = dlqr( A, B, 1.9*np.eye(4), 1.9*np.eye(2) )
-    u = np.matmul( np.array( K ), y )
-    print 'u=', u, 1000*u[0]/u[1]
-    # code.interact( local=locals() )
-    # return -5000.*u[0]/u[1]
-    return u
-
-# Pendulum and cart system. The motors on the cart turned at fixed time. In other words
+# Pendulum and cart system (non-linear eq). The motors on the cart turned at fixed time. In other words
 # The motors are actuated to deliver forward x force from t=t1 to t=t2.
 # Y : [ x, x_dot, theta, theta_dot]
 # Return \dot(Y)
-def func3( t, y ):
+def y_dot( t, y ):
     g = 9.8 # Gravitational Acceleration
     L = 1.5 # Length of pendulum
 
     m = 1.0 #mass of bob (kg)
     M = 5.0  # mass of cart (kg)
 
+    d1 = 1.0
+    d2 = 0.5
+
 
     x_ddot = u(t, y) - m*L*y[3]*y[3] * np.cos( y[2] ) + m*g*np.cos(y[2]) *  np.sin(y[2])
     x_ddot = x_ddot / ( M+m-m* np.sin(y[2])* np.sin(y[2]) )
 
-    theta_ddot = -g/L * np.cos( y[2] ) - 1./L * np.sin( y[2] ) * x_ddot
+    theta_ddot = -g/L * np.cos( y[2] ) -  np.sin( y[2] ) / L * x_ddot
 
-    damping_x =  - 1.0*y[1]
-    damping_theta =  - 0.5*y[3]
+
+    damping_x =  - d1*y[1]
+    damping_theta =  - d2*y[3]
 
     return [ y[1], x_ddot + damping_x, y[3], theta_ddot + damping_theta ]
+
+if __name__=="__main__":
+
+    # Verifying the correctness of linearization by evaluating y_dot
+    # in two ways a) non-linear b) linear approximation
+
+    at_y = np.array( [0,0,np.pi/2+0.1,0.002] )
+    print 'non-linear', y_dot( 1.0, at_y )
+    print 'linearized ', np.matmul( ss.A, at_y - np.array([0,0,np.pi/2.,0]) )  + ss.B.T * 0.1
+    code.interact(local=dict(globals(), **locals()))
 
 
 
 # Both cart and the pendulum can move.
-if __name__=="__main__":
+if __name__=="__1main__":
     # We need to write the Euler-lagrange equations for the both the
     # systems (bob and cart). The equations are complicated expressions. Although
     # it is possible to derive with hand. The entire notes are in media folder or the
     # blog post for this entry. Otherwse in essense it is very similar to free_fall_pendulum.py
     # For more comments see free_fall_pendulum.py
-    sol = solve_ivp(func3, [0, 20], [ 0.0, 0., np.pi/2 + 0.01, 0. ],   t_eval=np.linspace( 0, 20, 300)  )
+    sol = solve_ivp(y_dot, [0, 10], [ 0.0, 0., np.pi/2 + 0.01, 0. ],   t_eval=np.linspace( 0, 10, 100)  )
 
 
     syst = InvertedPendulum()
@@ -130,5 +107,5 @@ if __name__=="__main__":
         cv2.imshow( 'im', rendered )
         cv2.moveWindow( 'im', 100, 100 )
 
-        if cv2.waitKey(30) == ord('q'):
+        if cv2.waitKey(0) == ord('q'):
             break
